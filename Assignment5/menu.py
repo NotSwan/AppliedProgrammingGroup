8266afafb4a067c7e3dfaff4f147decc101fdd09
NotSwan <31709744+NotSwan@users.noreply.github.com>
2022-07-12 14:02:35 -0400

    Dummy Login

--- Assignment5/menu.py
+++ Assignment5/menu.py
@@ -1,25 +1,77 @@
-#Will be the main file 
+import sqlite3 as sql
+import Assignment1_TylerSwan
+# Contributors: Tyler Swan
+# Variables:
+# Use:
+#
+#
+def main():
+    try:
+        sql.connect("Assignment5/userList.db")
+    except:
+        pass
+    mainMenu()
 
-def menu():
-    pass
+# Contributors: Tyler Swan
+# Variables:
+# Use:
+#
+#
+def mainMenu():
+    print("Welcome to The School Managment System")
+    print("Please log on to continue")
+    logIO(True)
 
+# Contributors: 
+# Variables:
+# Use:
+#
+#
 def addRemoveClass():
     pass
 
+# Contributors: 
+# Variables:
+# Use:
+#
+#
 def printInstRoster():
     pass
 
+# Contributors: 
+# Variables:
+# Use:
+#
+#
 def addRemoveCourseAdmin():
     pass
 
-def logIO():
-    pass
+# Contributors: Tyler Swan
+# Variables:
+# Use:
+#
+#
+def logIO(logIn):
+    if logIn:
+        user = input("\nEnter username: ")
+        passW = input("\nEnter password: ")
+        print(user + " " + passW)
 
+# Contributors: 
+# Variables:
+# Use:
+#
+#
 def search():
     pass
 
+# Contributors: 
+# Variables:
+# Use:
+#
+#
 def editClass():
     pass
 
-if __name__ == __main__:
-    menu()
+if __name__ == '__main__':
+    main()
